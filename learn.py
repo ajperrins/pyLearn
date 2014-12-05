2f4920680efe8bd493d03a449a91f39b4a789735
Andrew Perrins <andrew.perrins@gmail.com>
2014-12-05 16:05:27 -0500

    Fixes #1

--- learn.py
+++ learn.py
@@ -8,25 +8,28 @@ Attributes:
 """
 
 import sys
+from urllib.request import urlopen
 
 def print_word_count(url):
 	""" Prints a count of words taken from a web resource ident'd by URL
 	 	Try https://www.gutenberg.org/cache/epub/47529/pg47529.txt, for example
 	"""
 	wc = {}
-	from urllib.request import urlopen
 	with urlopen(url) as story:
 		for line in story:
 			line_words = line.decode('utf-8').split()
 			for word in line_words:
-				wc.setdefault(word,0)
+				wc.setdefault(word, 0)
 				wc[word] += 1
-	print(wc)
+	
+	most_used = 0
+	most_used_word = ''
+	for word_key in wc:
+		if(wc[word_key] > most_used):
+			most_used = wc[word_key]
+			most_used_word = word_key
 
-
-def print_list(l):
-	for i in l.sort():
-		print(i)
+	print('{} is used {} times'.format(most_used_word, wc[most_used_word]))
 
 
 if __name__ == '__main__':
